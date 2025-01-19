257ee7fd04d31dd4c4bd8c687fc1e2f6d90658be
lyanych <lianna99994@gmail.com>
2025-01-20 00:05:44 +1000

    Исправлена строка подключения

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const app = express();
 
 // Подключение к MongoDB через Mongoose
-mongoose.connect("mongodb://admin:pass1234@task-manager-cluster-shard-00-00.mongodb.net:27017,task-manager-cluster-shard-00-01.mongodb.net:27017,task-manager-cluster-shard-00-02.mongodb.net:27017/task_manager?ssl=true&replicaSet=atlas-xyz-shard-0&authSource=admin&retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://admin:pass1234@task-manager-cluster.mongodb.net/task_manager?retryWrites=true&w=majority")
+
 
     .then(() => console.log("Подключено к MongoDB"))
     .catch(err => console.error("Ошибка подключения:", err));
